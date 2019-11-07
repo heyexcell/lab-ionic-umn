ab2af47895d5dd6e3a90f0bb977ac9934c036896
heyexcell <natanael.wongso@student.umn.ac.id>
2019-11-07 23:48:39 +0700

    week10

---
+++ w10-php-mysql/src/app/home/new-booking/booking.interface.ts
@@ -0,0 +1,11 @@
+export interface Booking {
+    booking_id: string;
+    bookings_name: string;
+    topic: string;
+    details: string;
+    booking_date: string;
+    start_hour: string;
+    end_hour: string;
+    status: number;
+    creator: string;
+}
---
+++ week-10/ionic-backend/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC6gesmljQkVYvy7chH59920q09KV-In5Y",
+    authDomain: "ionic-backend-9e8a1.firebaseapp.com",
+    databaseURL: "https://ionic-backend-9e8a1.firebaseio.com",
+    projectId: "ionic-backend-9e8a1",
+    storageBucket: "ionic-backend-9e8a1.appspot.com",
+    messagingSenderId: "1052430894913",
+    appId: "1:1052430894913:web:06497ab7a6662aea0283c0"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
