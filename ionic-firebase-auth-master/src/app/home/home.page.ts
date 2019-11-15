b0dca125ea1e209ca6bbe0d6696b660229d5f721
heyexcell <natanael.wongso@student.umn.ac.id>
2019-11-15 15:15:06 +0700

    week12

--- ionic-firebase-auth-master/src/app/home/home.page.ts
+++ ionic-firebase-auth-master/src/app/home/home.page.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { PlaceService } from './place.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  address = '';
 
-  constructor(private router: Router, private authSvc: AuthService) { }
+  constructor(
+    private router: Router, 
+    private authSvc: AuthService,
+    private placeSvc: PlaceService) { }
+
+    ngOnInit() {
+      this.placeSvc.getAddress().subscribe(currAddress => {
+          this.address = currAddress;
+        }
+      )
+    }
 
   onLogout() {
     this.authSvc.logout();
